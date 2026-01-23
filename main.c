9630ca02ee03ce6942546a8ba6a0065a457c11b2
GIKExe <i.gortolomei@yandex.ru>
2026-01-23 14:32:53 +0300

    змейка на односвязном списке

--- main.c
+++ main.c
@@ -15,68 +15,148 @@ int window_height = 600;
 
 #define FONT_PATH "ttf/consola.ttf"
 
+typedef   signed char i8;
 typedef unsigned char u8;
+typedef   signed short i16;
+typedef unsigned short u16;
+typedef   signed int i32;
+typedef unsigned int u32;
+typedef   signed long i64;
+typedef unsigned long u64;
+typedef float f32;
+typedef double f64;
 
 typedef struct {
-	int x, y;
+	i16 x, y;
 } Point;
 
-void set_random_pos(Point *pos) {
+typedef struct {
+	u8 r, g, b;
+} Color;
+
+typedef struct {
+	u8 x, y;
+} Pos;
+
+typedef struct SnakeElement SnakeElement;
+typedef struct SnakeElement {
+	Pos pos;
+	SnakeElement *pred;
+} SnakeElement;
+
+typedef struct {
+	Pos pos;
+	Color *color;
+} Apple;
+
+void set_random_pos(Pos *pos) {
 	pos->x = rand() % (window_widht / GRID_SIZE);
 	pos->y = rand() % (window_height / GRID_SIZE);
 }
 
-void draw_text(SDL_Renderer *renderer, TTF_Font *font, char *text, u8 x, u8 y, u8 r, u8 g, u8 b) {
-	SDL_Color color = {r, g, b, 255};
-	SDL_Surface *surface = TTF_RenderText_Blended(font, text, strlen(text), color);
+void draw_text(SDL_Renderer *renderer, TTF_Font *font, char *text, u8 x, u8 y, Color *color, bool w_centered, bool h_centered) {
+	SDL_Color _color = {color->r, color->g, color->b, 255};
+	SDL_Surface *surface = TTF_RenderText_Blended(font, text, strlen(text), _color);
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
 	SDL_FRect srcrect = {0, 0, surface->w, surface->h};
 	SDL_FRect dstrect = {x, y, surface->w, surface->h};
+	if (w_centered) dstrect.x = window_widht / 2 - surface->w / 2 + x;
+	if (h_centered) dstrect.y = window_height / 2 - surface->h / 2 + y;
 
 	SDL_RenderTexture(renderer, texture, &srcrect, &dstrect);
 	SDL_DestroySurface(surface);
 	SDL_DestroyTexture(texture);
 }
 
-int main(int argc, char* argv[]) {
-	// 0. Инициализация Рандома
-	srand(time(NULL));
+bool is_collide(Pos *a, Pos *b) {
+	return a->x == b->x && a->y == b->y;
+}
 
-	// 1. Инициализация SDL3
-	SDL_Init(SDL_INIT_VIDEO);
-	TTF_Init();
-	TTF_Font *font = TTF_OpenFont(FONT_PATH, 16);
-	if (font == NULL) {
-		printf("Не удалось загрузить шрифт: ", FONT_PATH);
-		exit(0);
-	}
-	
-	// 2. Создание окна и рендерера
-	SDL_Window* window = SDL_CreateWindow("Змейка", window_widht, window_height, SDL_WINDOW_RESIZABLE);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-	
-	// 3. Основные переменные
+typedef struct {
+	TTF_Font *font16;
+	TTF_Font *font20;
+	TTF_Font *font48;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} Core;
+
+bool exit_flag = false;
+Color ColorRed = {255, 0, 0};
+Color ColorGreen = {0, 255, 0};
+Color ColorGold = {255, 182, 79};
+
+SnakeElement *new_element(SnakeElement *end_of_snake) {
+	SnakeElement *element = malloc(sizeof(SnakeElement));
+	element->pos.x = end_of_snake->pos.x;
+	element->pos.y = end_of_snake->pos.y;
+	element->pred = end_of_snake;
+	return element;
+}
+
+void death_menu(Core *core) {
 	bool is_running = true;
 	SDL_Event event;
-	
-	// Пример: инициализация змейки
-	Point snake[100];
-	set_random_pos(&snake[0]);
-	int snake_length = 1;
-	int direction = 0; // 0=right, 1=down, 2=left, 3=up
-	Point food;
-	set_random_pos(&food);
-	
+
 	while (is_running) {
 		while (SDL_PollEvent(&event)) {
 			switch (event.type) {
 				case SDL_EVENT_QUIT:
 					is_running = false;
+					exit_flag = true;
+					break;
+				case SDL_EVENT_KEY_DOWN:
+					switch (event.key.key) {
+						case SDLK_ESCAPE:
+							exit_flag = true;
+						case SDLK_SPACE:
+							is_running = false;
+							break;
+					}
+					break;
+			}
+		}
+
+		SDL_SetRenderDrawColor(core->renderer, 0, 0, 0, 255);
+		SDL_RenderClear(core->renderer);
+
+		char msg1[] = "Змейка сдохла!";
+		draw_text(core->renderer, core->font48, (char *) &msg1, 0, 0, &ColorRed, true, true);
+		char msg2[] = "нажмите пробел чтобы начать";
+		draw_text(core->renderer, core->font20, (char *) &msg2, 0, 40, &ColorRed, true, true);
+
+		SDL_RenderPresent(core->renderer);
+		SDL_Delay(100);
+	}
+}
+
+void game_menu(Core *core) {
+	bool is_running = true;
+	SDL_Event event;
+	
+	// Пример: инициализация змейки
+	SnakeElement *snake = malloc(sizeof(SnakeElement));
+	SnakeElement *end_of_snake = snake;
+	SnakeElement *element;
+	SnakeElement *element_pred;
+	snake->pred = NULL;
+	set_random_pos(&snake->pos);
+	int direction = 0; // 0=right, 1=down, 2=left, 3=up
+	Apple food;
+	food.color = &ColorRed;
+	set_random_pos(&food.pos);
+	
+	while (is_running) {
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_EVENT_QUIT:
+					exit_flag = true;
+					is_running = false;
 					break;
 				case SDL_EVENT_KEY_DOWN:
 					switch(event.key.key) {
 						case SDLK_ESCAPE:
+							exit_flag = true;
 							is_running = false;
 							break;
 						case SDLK_W:
@@ -103,70 +183,130 @@ int main(int argc, char* argv[]) {
 					break;
 			}
 		}
-		
-		// Логика движения змейки
-		for (int i = snake_length - 1; i > 0; i--) {
-			snake[i] = snake[i-1];
+
+		// Проверка съедения еды
+		if (is_collide(&snake->pos, &food.pos)) {
+			end_of_snake = new_element(end_of_snake);
+			if (food.color == &ColorGold) {
+				end_of_snake = new_element(end_of_snake);
+			}
+			set_random_pos(&food.pos);
+			food.color = (rand() % 100 < 10 ? &ColorGold : &ColorRed);
 		}
 		
-		// Перемещение головы
-		switch(direction) {
-			case 0: snake[0].x++; break; // right
-			case 1: snake[0].y++; break; // down
-			case 2: snake[0].x--; break; // left
-			case 3: snake[0].y--; break; // up
+		// Логика движения змейки (с конца)
+		element = end_of_snake;
+		element_pred = element->pred;
+		while (element_pred != NULL) {
+			element->pos.x = element_pred->pos.x;
+			element->pos.y = element_pred->pos.y;
+			element = element_pred;
+			element_pred = element->pred;
 		}
 
-		if (snake[0].x > WWL) snake[0].x = 0;
-		if (snake[0].x < 0) snake[0].x = WWL;
-		if (snake[0].y > WHL) snake[0].y = 0;
-		if (snake[0].y < 0) snake[0].y = WHL;
+		// Перемещение головы
+		switch(direction) {
+			case 0: snake->pos.x++; break; // right
+			case 1: snake->pos.y++; break; // down
+			case 2: snake->pos.x--; break; // left
+			case 3: snake->pos.y--; break; // up
+		}
+
+		if (snake->pos.x == 255) snake->pos.x = WWL;
+		if (snake->pos.x > WWL) snake->pos.x = 0;
+		if (snake->pos.y == 255) snake->pos.y = WHL;
+		if (snake->pos.y > WHL) snake->pos.y = 0;
 		
-		// Проверка съедения еды
-		if (snake[0].x == food.x && snake[0].y == food.y) {
-			snake[snake_length] = snake[snake_length - 1];
-			snake_length++;
-			set_random_pos(&food);
+		// обработка сталкивания 
+		element = end_of_snake;
+		while (element != NULL && element != snake) {
+			if (is_collide(&snake->pos, &element->pos)) {
+				is_running = false;
+				break;
+			}
+			element = element->pred;
 		}
 		
 		// Отрисовка
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		
-		// Отрисовка змейки
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-		for (int i = 0; i < snake_length; i++) {
-			SDL_FRect rect = {
-				snake[i].x * GRID_SIZE,
-				snake[i].y * GRID_SIZE,
-				GRID_SIZE,
-				GRID_SIZE
-			};
-			SDL_RenderFillRect(renderer, &rect);
-		}
-		
+		SDL_SetRenderDrawColor(core->renderer, 0, 0, 0, 255);
+		SDL_RenderClear(core->renderer);
+
 		// Отрисовка еды
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_SetRenderDrawColor(core->renderer, food.color->r, food.color->g, food.color->b, 255);
 		SDL_FRect food_rect = {
-			food.x * GRID_SIZE,
-			food.y * GRID_SIZE,
+			food.pos.x * GRID_SIZE,
+			food.pos.y * GRID_SIZE,
 			GRID_SIZE - 1,
 			GRID_SIZE - 1
 		};
-		SDL_RenderFillRect(renderer, &food_rect);
+		SDL_RenderFillRect(core->renderer, &food_rect);
+		
+		// Отрисовка змейки
+		SDL_SetRenderDrawColor(core->renderer, 0, 255, 0, 255);
+		element = end_of_snake;
+		while (element != NULL) {
+			SDL_FRect rect = {
+				element->pos.x * GRID_SIZE,
+				element->pos.y * GRID_SIZE,
+				GRID_SIZE - 1,
+				GRID_SIZE - 1
+			};
+			SDL_RenderFillRect(core->renderer, &rect);
+			element = element->pred;
+		}
 
-		char result[29];
-		snprintf(result, sizeof(result), "Позиция: %hd, %hd", snake[0].x, snake[0].y);
-		draw_text(renderer, font, (char *) &result, 5, 5, 255, 0, 0);
+		char result[25];
+		snprintf(result, sizeof(result), "Позиция: %hhd, %hhd", snake->pos.x, snake->pos.y);
+		draw_text(core->renderer, core->font16, (char *) &result, 5, 5, &ColorRed, false, false);
 
-		SDL_RenderPresent(renderer);
+		SDL_RenderPresent(core->renderer);
 		SDL_Delay(100);
 	}
+
+	// освобождение памяти
+	element = end_of_snake;
+	while (element != NULL) {
+		element_pred = element->pred;
+		free(element);
+		element = element_pred;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	// Инициализация Рандома
+	srand(time(NULL));
+
+	// Инициализация SDL3
+	SDL_Init(SDL_INIT_VIDEO);
+	TTF_Init();
+
+	// Сбор данных для расшаривания
+	Core core;
+	core.font16 = TTF_OpenFont(FONT_PATH, 16);
+	core.font20 = TTF_OpenFont(FONT_PATH, 20);
+	core.font48 = TTF_OpenFont(FONT_PATH, 48);
+	if (core.font16 == NULL) {
+		printf("Не удалось загрузить шрифт: ", FONT_PATH);
+		exit(0);
+	}
 	
-	// 5. Очистка
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	TTF_CloseFont(font);
+	// Создание окна и рендерера
+	core.window = SDL_CreateWindow("Змейка", window_widht, window_height, SDL_WINDOW_RESIZABLE);
+	core.renderer = SDL_CreateRenderer(core.window, NULL);
+
+	while (true) {
+		game_menu(&core);
+		if (exit_flag) break;
+		death_menu(&core);
+		if (exit_flag) break;
+	}
+	
+	// Очистка
+	SDL_DestroyRenderer(core.renderer);
+	SDL_DestroyWindow(core.window);
+	TTF_CloseFont(core.font16);
+	TTF_CloseFont(core.font20);
+	TTF_CloseFont(core.font48);
 	TTF_Quit();
 	SDL_Quit();
 	
